ebd42a142727c641d1b62512848cd55bdc78255a
Angela Zhou <angelazhou922@gmail.com>
2018-09-18 13:38:27 -0700

    Finalized

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,18 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, node) {
+  var arr = [];
+  if(node === undefined){
+    node = document.body;
+  }
+  if(node.classList && node.classList.contains(className)){
+    arr.push(node)
+  }
+  for(var i = 0; i < node.childNodes.length; i++){
+    var b = node.childNodes[i];
+    arr = arr.concat(getElementsByClassName(className, b));
+  }
+  return arr;
 };
+getElementsByClassName('targetClassName')
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,55 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (obj === undefined) {
+    return undefined;
+  }
+
+  if (obj === null) {
+    return "null";
+  }
+
+  if (typeof obj === "number" || typeof obj === "boolean") {
+    return obj.toString();
+  }
+
+  if (typeof obj === "string") {
+    return '"' + obj + '"';
+  }
+
+  if (Array.isArray(obj)) {
+    let firstElement = "[";
+    let lastIndex = obj.length-1;
+    if (obj.length > 0) {
+      for (let i = 0; i < obj.length; i++) {
+        firstElement += stringifyJSON(obj[i]);
+        if (i !== lastIndex) {
+          firstElement += ","; 
+        }
+      }
+      return firstElement + "]";
+    } else {
+      return "[]"
+    } 
+  }
+  
+  if (typeof obj=== "object") {
+    var firstBracket = "{";
+    var objectKeys = Object.keys(obj); //"one", "two", "three"
+    var lastIndex = objectKeys.length-1;
+    if (objectKeys.length > 0) {
+      for (var i = 0; i < objectKeys.length; i++) {
+        if (obj[objectKeys[i]] === undefined || typeof obj[objectKeys[i]]  === 'function') {
+          return '{}';
+        }
+        firstBracket += stringifyJSON(objectKeys[i]) + ":" + stringifyJSON(obj[objectKeys[i]]);
+        if (i !== lastIndex) {
+          firstBracket += ",";
+        }
+      }
+      return firstBracket + "}";
+    } else {
+      return "{}";
+    }
+  }
 };
